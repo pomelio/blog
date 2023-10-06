accaf51df13405ade5d22cd7bb1a1619468af7f3
jaynsw <125770399+chatsarah@users.noreply.github.com>
2023-10-06 11:26:54 +1100

    df

--- public/js/follower-pannel.js
+++ public/js/follower-pannel.js
@@ -5,7 +5,7 @@ $(document).on('my-account', (event, account) => {
 
     function renderHTML() {
         let html = `
-    <button type="button" class="followers relative inline-flex items-center p-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+    <button type="button" class="follower-toggle relative inline-flex items-center p-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Followers
         <span class="sr-only">Notifications</span>
         <div class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">${project.followers.length}</div>
@@ -16,7 +16,7 @@ $(document).on('my-account', (event, account) => {
             var currentURL = encodeURIComponent(window.location.href);
             var loginURL = `/login?redirect=${currentURL}`;
             html = `
-        <a href="${loginURL}" type="button" class="followers relative inline-flex items-center p-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <a href="${loginURL}" type="button" class="follower-toggle relative inline-flex items-center p-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Followers
             <span class="sr-only">Notifications</span>
             <div class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">${project.followers.length}</div>
@@ -31,7 +31,7 @@ $(document).on('my-account', (event, account) => {
     let html = renderHTML();
     $(component_id).replaceWith(html);
 
-    $(".followers").on("click", event => {
+    $(".follower-toggle").on("click", event => {
         event.preventDefault();
         
        
