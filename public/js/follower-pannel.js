0af67a06a003a5348c892ea86d4938986e9de2ad
jaynsw <125770399+chatsarah@users.noreply.github.com>
2023-10-06 12:32:39 +1100

    df

--- public/js/follower-pannel.js
+++ public/js/follower-pannel.js
@@ -16,7 +16,7 @@ $(document).on('my-account', (event, account) => {
             var currentURL = encodeURIComponent(window.location.href);
             var loginURL = `/login?redirect=${currentURL}`;
             html = `
-        <a href="${loginURL}" type="button" class="follower-toggle relative inline-flex items-center p-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <a href="${loginURL}" type="button" class="relative inline-flex items-center p-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Followers
             <span class="sr-only">Notifications</span>
             <div class="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">${followers.length}</div>
